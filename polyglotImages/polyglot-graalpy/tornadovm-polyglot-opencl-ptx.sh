63cc2c1d088ec2e5236ede085b7285eeb5f197f3
Thanos Stratikopoulos <athanasios.stratikopoulos@manchester.ac.uk>
2025-04-03 11:20:16 +0300

    Update flag for docker runtime

--- polyglotImages/polyglot-graalpy/tornadovm-polyglot-opencl-ptx.sh
+++ polyglotImages/polyglot-graalpy/tornadovm-polyglot-opencl-ptx.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
 if [[ "$1" == "--console" ]]; then
-    docker run -it -p 8080:8080 --rm --runtime=nvidia --gpus all -v "$PWD":/data beehivelab/tornadovm-polyglot-graalpy-23.1.0-opencl-cuda-container:tango-v1.1.0
+    docker run -i -p 8080:8080 --rm --runtime=nvidia --gpus all -v "$PWD":/data beehivelab/tornadovm-polyglot-graalpy-23.1.0-opencl-cuda-container:tango-v1.1.0
 elif [[ "$1" == "--help" ]] || [[ "$1" == "--h" ]]; then
     echo "Please run:"
     echo "  ./tornadovm-polyglot-nvidia.sh --console		to launch the built image in which GraalPy interoperates with TornadoVM, or"
     echo "  ./tornadovm-polyglot-nvidia.sh <command>		to launch the built image or execute a Python program that interoperates with TornadoVM, or"
     echo "  ./tornadovm-polyglot-nvidia.sh --help		to print help message"
 else
-    docker run -it -p 8080:8080 --rm --runtime=nvidia --gpus all -v "$PWD":/data beehivelab/tornadovm-polyglot-graalpy-23.1.0-opencl-cuda-container:tango-v1.1.0 "$@"
+    docker run -i -p 8080:8080 --rm --runtime=nvidia --gpus all -v "$PWD":/data beehivelab/tornadovm-polyglot-graalpy-23.1.0-opencl-cuda-container:tango-v1.1.0 "$@"
 fi
